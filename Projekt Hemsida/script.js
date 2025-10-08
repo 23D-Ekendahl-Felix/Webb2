13a4bc66f6f7953de2bb2eb3906b43d3b17d5bf8
felix e <felix.ekendahl@elev.ga.ntig.se>
2025-10-08 14:58:21 +0200

    jobbat hemma med Hemsidan projektet

--- Projekt Hemsida/script.js
+++ Projekt Hemsida/script.js
@@ -12,7 +12,6 @@ var Spinul = document.querySelector(".SpinListconteiner");
 
 SubmitText.addEventListener("click", function () {
     if (Name.value == "" && Emailaddress.value == "" && Message.value == "") {
-        alert("Please fill in all fields");
     }
     else {
         var li = document.createElement("li");
@@ -25,21 +24,76 @@ SubmitText.addEventListener("click", function () {
 });
 
 Spin.addEventListener("click", function () {
-    for (let i = 0; i < Nummer.length; i++){
-    Nummer[i].textContent = Math.floor(Math.random() * 5) + 1;
+    for (let i = 0; i < Nummer.length; i++) {
+        var bet = 10;
+        if (pengar - bet < 0 || pengar <= 0) {
+            alert("You don't have enough money to spin!");
+            return;
+        }
+        pengar -= bet;
+        Pengar.textContent = pengar + " $";
+        Nummer[i].textContent = Math.floor(Math.random() * 5) + 1;
 
-    if (Nummer[i].textContent == 5) {
-        card[i].style.backgroundColor = "yellow";
+        if (Nummer[i].textContent == 1) {
+            card[i].style.backgroundColor = "yellow";
+        }
+        if (Nummer[i].textContent == 2) {
+            card[i].style.backgroundColor = "green";
+        }
+        if (Nummer[i].textContent == 3) {
+            card[i].style.backgroundColor = "red";
+        }
+        if (Nummer[i].textContent == 4) {
+            card[i].style.backgroundColor = "blue";
+        }
+        if (Nummer[i].textContent == 5) {
+            card[i].style.backgroundColor = "purple";
+        }
     }
-    else {
-        card[i].style.backgroundColor = "white";
+    if (Nummer[0].textContent == 5 && Nummer[1].textContent == 5 && Nummer[2].textContent == 5) {
+        pengar += bet * 10;
+        Pengar.textContent = pengar + " $";
     }
-}
-if (Nummer[0].textContent == 5 && Nummer[1].textContent == 5 && Nummer[2].textContent == 5) {
-    alert("You win!");
-}
-var li = document.createElement("li");
-li.textContent = "Nummer 1: " + Nummer[0].textContent + "  ||    Nummer 2: " + Nummer[1].textContent + "  ||    Nummer 3: " + Nummer[2].textContent;
-Spinul.appendChild(li);
+    var li = document.createElement("li");
+    li.textContent = "Nummer 1: " + Nummer[0].textContent + "  ||    Nummer 2: " + Nummer[1].textContent + "  ||    Nummer 3: " + Nummer[2].textContent;
+    Spinul.appendChild(li);
 
 });
+
+var SpinLog = document.querySelector(".SpinLog");
+var SpinLogModal = document.querySelector(".modal");
+var closeSpinLog = document.querySelector(".SpinLogclose");
+var NoSpinText = document.querySelector(".Feltext");
+var clearLog = document.querySelector(".clearLog");
+
+
+clearLog.addEventListener("click", function () {
+    if (Spinul.children.length == 0) {
+        NoSpinText.textContent = "You have not spun yet!";
+    }
+    else {
+        Spinul.innerHTML = "";
+        NoSpinText.textContent = "Log cleared!";
+        SpinLogModal.style.display = "none";
+    }
+});
+
+SpinLog.addEventListener("click", function () {
+    SpinLogModal.style.display = "block";
+    if (Spinul.children.length == 0) {
+        SpinLogModal.style.display = "none";
+        NoSpinText.textContent = "You have not spun yet!";
+    }
+    else {
+        NoSpinText.textContent = "";
+    }
+
+});
+closeSpinLog.addEventListener("click", function () {
+    SpinLogModal.style.display = "none";
+});
+
+var pengar = 1000;
+var Pengar = document.querySelector(".Pengar");
+Pengar.textContent = pengar + " $";
+
