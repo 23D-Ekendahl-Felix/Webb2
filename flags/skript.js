ed91532ba53663968e5642520d830e59a9bae20e
felix e <felix.ekendahl@elev.ga.ntig.se>
2025-10-22 09:49:17 +0200

    tränade på flaggor

--- flags/skript.js
+++ flags/skript.js
@@ -4,21 +4,31 @@ let knapp2 = document.querySelector('#btn-many');
 let knapp3 = document.querySelector('#btn-clear');
 
 
-knapp1.addEventListener('click', function() {
+knapp1.addEventListener('click', function () {
     fetch('http://10.151.168.5:8081/flags/random')
-    .then(svar => {
-        return svar.json();
-    })
-    .then(data => {
-        console.log(data.name, data.svgUrl);
- let urlbilden = 'http://10.151.168.5:8081' + data.svgUrl;
- flagContainer.insertAdjacentHTML('afterbegin', '<img src="' + urlbilden + '" alt="' + data.name + '" width="200">');
-    });
+        .then(svar => {
+            return svar.json();
+        })
+        .then(data => {
+            console.log(data.name, data.svgUrl);
+            let urlbilden = 'http://10.151.168.5:8081' + data.svgUrl;
+            flagContainer.insertAdjacentHTML('afterbegin', '<img src="' + urlbilden + '" alt="' + data.name + '" width="200">');
+        });
 });
-knapp2.addEventListener('click', function() {
-for (let i = 0; i < 18; i++) {
-    fetch('http://10.151.168.5:8081/flags/random')
+knapp2.addEventListener('click', function () {
+    for (let i = 0; i < 100; i++) {
+           fetch('http://10.151.168.5:8081/flags/random')
+        .then(svar => {
+            return svar.json();
+        })
+        .then(data => {
+            console.log(data.name, data.svgUrl);
+            let urlbilden = 'http://10.151.168.5:8081' + data.svgUrl;
+            flagContainer.insertAdjacentHTML('afterbegin', '<img src="' + urlbilden + '" alt="' + data.name + '" width="200">');
+        });
+    }
 });
-knapp3.addEventListener('click', function() {
-  flagContainer.innerHTML = '';
-});
+knapp3.addEventListener('click', function () {
+    flagContainer.innerHTML = '';
+}); 
+
