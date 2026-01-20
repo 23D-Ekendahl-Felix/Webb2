3ac94aa912cf6ad7c6044f78089cdd6ecd6d6398
felix e <felix.ekendahl@elev.ga.ntig.se>
2026-01-20 16:24:38 +0100

    tränad penna

---
+++ Kapitel5/canvas/script.js
@@ -0,0 +1,35 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+// // ctx.fillStyle = "blue";
+// // ctx.fillRect(50, 50, 150, 100);
+
+// // ctx.fillStyle = "red";
+// // ctx.fillRect(600, 50, 150, 100);
+
+// // ctx.fillStyle = "green";
+// // ctx.fillRect(200, 300, 400, 100);
+
+// // ctx.strokeStyle = "black";
+
+// ctx.fillStyle = "";
+// ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// ctx.fillStyle = "red";
+// ctx.fillRect(220, 0, 100, canvas.height);
+
+// ctx.fillStyle = "yellow";
+// ctx.fillRect(0, 200, canvas.width, 100);
+
+// ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = "lightblue";
+ctx.fillRect(0, 0, 200,100);
+ctx.fillRect(250, 0, 200,100);
+ctx.fillRect(0, 150, 200,100);
+ctx.fillRect(250, 150, 200,100);
+ctx.fillRect(300, 0, 500,50);
+ctx.fillRect(450, 100,700,50 );
+ctx.fillRect(300, 200,500,50  );
+ctx.fillRect(0, 300, canvas.width, 50);
+ctx.fillRect(0, 400, canvas.width, 50 );
---
+++ Kapitel5/penna/script.js
@@ -0,0 +1,73 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+// // ctx.strokeStyle = "orange";
+// // ctx.lineWidth = 25;
+// // ctx.beginPath();
+// // ctx.moveTo(100, 100);
+// // ctx.lineTo(200, 100);
+// // ctx.lineTo(200, 200);
+// // ctx.lineTo(300, 200);
+// // ctx.lineTo(300, 100);
+// // ctx.lineTo(400, 100);
+// // ctx.lineTo(400, 300);
+
+// // ctx.lineTo(100, 300);
+// // ctx.lineTo(100, 87.5);
+// // ctx.fillStyle = "yellow";
+// // ctx.fill();
+// // ctx.stroke();
+// // ctx.closePath();
+
+// // ctx.strokeStyle = "green";
+// // ctx.lineWidth = 10;
+// // ctx.beginPath();
+// // ctx.moveTo(100, 350);
+// // ctx.lineTo(400, 350);
+
+// // ctx.stroke();
+// // ctx.closePath();
+
+// //vi ska rita ett 3d hus med pennan
+// ctx.strokeStyle = "brown";
+// ctx.lineWidth = 5;
+// ctx.beginPath();
+
+// ctx.moveTo(150, 300);
+// ctx.lineTo(150, 200);
+// ctx.lineTo(300, 200);
+// ctx.lineTo(300, 300);
+// ctx.lineTo(150, 300);
+
+// ctx.moveTo(150, 200);
+// ctx.lineTo(225, 150);
+// ctx.lineTo(300, 200);
+// ctx.moveTo(225, 150);
+// ctx.lineTo(350, 100);
+// ctx.lineTo(400, 150);
+// ctx.lineTo(300, 200);
+
+// ctx.moveTo(300, 200);
+// ctx.lineTo(400, 150);
+// ctx.lineTo(400, 250);
+// ctx.lineTo(300, 300);
+
+// ctx.moveTo(200, 300);
+// ctx.lineTo(200, 250);
+// ctx.lineTo(250, 250);
+// ctx.lineTo(250, 300);
+// ctx.moveTo(225, 300);
+
+
+
+// ctx.stroke();
+// ctx.closePath();
+
+// //pac
+// ctx.beginPath();
+// ctx.arc(300, 150, 50, 0.5, 5.4);
+// ctx.lineTo(300, 150);
+// ctx.fillStyle = "yellow";
+// ctx.fill();
+// ctx.closePath();
+// ctx.strokeStyle = "black";
